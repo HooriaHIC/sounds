04d654834c80e692ce88ea839c7843ac62ae49bf
HooriaHIC <hooria.hic@gmail.com>
2021-10-06 18:29:54 +0500

    updated django

--- users/models.py
+++ users/models.py
@@ -5,7 +5,7 @@ from PIL import Image
 
 class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE, null=True)
-    image = models.ImageField(default='', null=True, upload_to='profile_pics')
+    image = models.ImageField(default="", null=True, upload_to='profile_pics')
     real_name = models.CharField(max_length=150)
     bio = models.TextField(default="", null=True)
     age = models.IntegerField(default=None, null=True)
@@ -15,9 +15,10 @@ class Profile(models.Model):
 
     def save(self, *args, **kwargs):
         super().save()
-        img = Image.open(self.image.path)
+        if self.image:
+            img = Image.open(self.image.path)
 
-        if img.height > 300 or img.width > 300:
-            output_size = (300, 300)
-            img.thumbnail(output_size)
-            img.save(self.image.path)
+            if img.height > 300 or img.width > 300:
+                output_size = (300, 300)
+                img.thumbnail(output_size)
+                img.save(self.image.path)
